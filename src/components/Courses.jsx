2a5615ac96e02bd748e2d6ea6c4afa05887c0421
Paparapy <HasiniainaRakotomanana.skype@gmail.com>
2024-10-02 13:46:05 +0300

    projet demmarer

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -7,7 +7,7 @@ function Courses() {
   return (
     <>
       <div className="container px-4 mx-auto max-w-screen-2xl md:px-20">
-        <div className="justify-center text-center  mt-28 align-center">
+        <div className="justify-center text-center mt-28 align-center">
           <h1 className="text-2xl md:text-4xl">
             Je vous donnes, <span className="text-pink-500">Vous avez vu?</span>
           </h1>
@@ -17,8 +17,6 @@ function Courses() {
             quam repellat laboriosam fugiat corporis sunt. Nulla mollitia dolor
             beatae hic. Lorem ipsum dolor sit amet consectetur, adipisicing
             elit. Unde ipsa odio alias aut. Tempore, magni. Minima ratione minus
-            perferendis quam repellat laboriosam fugiat corporis sunt. Nulla
-            mollitia dolor beatae hic.
           </p>
             <Link to="/">
             <button className="bg-pink-500 rounded-lg text-white px-4 py-1 mt-8 hover:bg-pink-600 hover:text-white border-[1px] border-pink-500 duration-200">
